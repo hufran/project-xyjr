3d18b61d8038bf79e4a58232f5e73e04796fd5cd
Anson Yu Wang <opensource@undozen.com>
2015-04-27 12:20:29 +0800

    upgrade oauth2

--- oauth2/additional-endpoints.js
+++ oauth2/additional-endpoints.js
@@ -1,7 +1,4 @@
 'use strict';
-var auth = require('@cc/oauth2').auth;
-var router = require('express').Router();
-module.exports = router;
-
-router.get('/api/v0/hello/world', auth.pass());
-router.get('/api/v0/hello/:userID', auth.user());
+module.exports = function (router, auth) {
+    router.get('/api/v0/hello/world', auth.pass());
+};
