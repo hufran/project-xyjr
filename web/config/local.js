2882effd6c65e2ba53b073477ea4c469e9aa6f16
immissile <shaohualou@gmail.com>
2015-04-29 11:47:05 +0800

    fix

--- web/config/local.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-    urlBackend: "http://123.59.61.50",
-    oauth2client: {
-        id: "a266a555-856c-49d9-8941-f1d68a03c551",
-        secret: "726bfcfdb267ea0fd971481640c9efe59a3786d6d961e96296eb9229f42dd1f9"
-    }
-};
