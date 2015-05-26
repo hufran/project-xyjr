84421fe2ee02baf369b9e9d851f64ddc9d329704
jiuxin1 <304299870@qq.com>
2015-05-26 17:25:22 +0800

    修改样式

--- web/ccc/company/router.js
+++ web/ccc/company/router.js
@@ -2,10 +2,10 @@
 var router = module.exports = require('@ds/base')
     .createSubApp(__dirname);
 
-router.get('/company/hanhua', function (req, res) {
+/*router.get('/company/hanhua', function (req, res) {
     var user = res.locals.user;
     res.expose(user, "user");
     res.render('company/hanhua', {
         title: '瀚华担保简介'
     });
-});
+});*/
--- web/ccc/register/router.js
+++ web/ccc/register/router.js
@@ -5,7 +5,7 @@ var router = module.exports = require('@ds/base')
 router.get('/register*', function(req, res, next) {
     res.locals.CLEAN_PAGE = true;
     res.locals.titlePrefix = '注册';
-    
+    res.locals.title = '用户注册|九信金融-国内首家PE系互联网金融平台';
     next();
 });
 
