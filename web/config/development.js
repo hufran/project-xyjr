ca1556672e5064450d28b3bd51fcac56a705b064
 ct <lifepool@163.com>
2015-07-22 18:21:14 +0800

    登陆注册

--- web/config/development.js
+++ web/config/development.js
@@ -15,4 +15,5 @@ module.exports = {
         id: "a266a555-856c-49d9-8941-f1d68a03c551",
         secret: "726bfcfdb267ea0fd971481640c9efe59a3786d6d961e96296eb9229f42dd1f9"
     }
+//    urlBackend: "http://ysjr.uats.cc:4100/",
 };
