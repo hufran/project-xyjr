51f52f3804d8baf2e4df35e3427fed9b4a454012
jason <jason.xu@creditcloud.com>
2015-08-03 16:10:51 +0800

    add lianlianpay route

--- oauth2/additional-endpoints.js
+++ oauth2/additional-endpoints.js
@@ -2,4 +2,10 @@
 module.exports = function (router, auth) {
     router.get('/api/v0/hello/world', auth.pass());
     router.get('/api/v2/message/notifications/:page/:userId', auth.owner());
+
+
+    // 支付相关
+    router.post('/api/v2/lianlianpay/bindCard/:userId', auth.user());
+    router.post('/api/v2/lianlianpay/deposit/:userId', auth.user());
+    router.post('/api/v2/lianlianpay/withdraw/:userId', auth.user());
 };
