d3174e4ade9a0248a2711e70fff0c6aac4e0c226
wei <317505615@qq.com>
2015-08-06 14:24:15 +0800

    借款意向bug

--- oauth2/additional-endpoints.js
+++ oauth2/additional-endpoints.js
@@ -8,7 +8,8 @@ module.exports = function (router, auth) {
     router.post('/api/v2/lianlianpay/bindCard/:userId', auth.user());
     router.post('/api/v2/lianlianpay/deposit/:userId', auth.user());
     router.post('/api/v2/lianlianpay/withdraw/:userId', auth.user());
-
+    router.post('/api/v2/loanIntent/addNew', auth.pass());
+    router.get('/api/v2/loanIntent/:userId/listAll', auth.user());
     router.get('/api/v2/lianlianpay/banks', auth.user());
     router.get('/api/v2/lianlianpay/provinceCodes', auth.user());
     router.get('/api/v2/lianlianpay/provinceCityCodes/:provinceName', auth.user());
