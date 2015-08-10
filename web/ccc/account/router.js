2ef31b7e0ab7f4824ac025c94d28ae42095042ed
lucky7ky <kaiyi0707@163.com>
2015-08-10 17:14:49 +0800

    fix 邮箱认证

--- web/ccc/account/router.js
+++ web/ccc/account/router.js
@@ -336,4 +336,26 @@ router.get('/account/allContracts/:id',
             '/contract');
         next();
     });
+
+//邮箱验证
+router.get('/account/authenticateEmail', function(req,res,next) {
+
+    var email = req.query.email;
+    var code = req.query.code;
+    
+    var sendObj = {
+        code : code,
+        email : email
+     };
+
+     req.uest.post('/api/v2/user/authenticateEmail')
+        .type('form')
+        .send(sendObj)
+        .end()
+        .then(function(r) {
+         
+            res.redirect('/register/renzheng?message=' + r.body.ConfirmResult);
+         });
+ });
 }
+
