fa40553c8d41582a95e39375712922705caf72cb
jason <jason.xu@creditcloud.com>
2015-08-10 16:09:17 +0800

    add route

--- oauth2/additional-endpoints.js
+++ oauth2/additional-endpoints.js
@@ -9,16 +9,19 @@ module.exports = function (router, auth) {
     router.post('/api/v2/lianlianpay/deposit/:userId', auth.user());
     router.post('/api/v2/lianlianpay/withdraw/:userId', auth.user());
     router.post('/api/v2/lianlianpay/depositReturn', auth.user());
-
-    router.post('/api/v2/loanIntent/addNew', auth.pass());
-    router.get('/api/v2/loanIntent/:userId/listAll', auth.user());
     router.get('/api/v2/lianlianpay/banks', auth.user());
     router.get('/api/v2/lianlianpay/provinceCodes', auth.user());
     router.get('/api/v2/lianlianpay/provinceCityCodes/:provinceName', auth.user());
-    router.get('/api/v2/loan/getLoanProduct/productKey/:productKey', auth.user());
     router.post('/api/v2/lianlianpay/authenticateUser/:userId', auth.user());
 
+    router.post('/api/v2/loanIntent/addNew', auth.pass());
+    router.get('/api/v2/loanIntent/:userId/listAll', auth.user()); 
+    router.get('/api/v2/loan/getLoanProduct/productKey/:productKey', auth.user());
+    
+    router.get('/api/v2/message/markAsRead/:messageId', auth.user());
+    router.get('/api/v2/message/user/:userId/listByStatus', auth.user());
     router.get('/api/v2/message/user/:userId/notifications', auth.user());
+    
     router.get('/api/v2/user/:userId/invite', auth.user());
     router.post('/api/v2/users/mobile/encrypt', auth.pass());
 };
