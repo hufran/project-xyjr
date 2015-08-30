d2ff7a832c3bb4cd68153bd858df400ddd51a5c9
Anson Yu Wang <opensource@undozen.com>
2015-08-30 23:17:26 +0800

    add ccc/wx

---
+++ web/config/local.sample.wx.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+    weixinmp: { // 微信公共帐号绑定的相关配置
+        appid: 'wx996e9ad463045dc3',
+        secret: '0aa050f2fcbfdcda4eceabfc393846c0',
+        token: 'creditcloud',
+        useUnionId: false, // 默认只使用 openId，打开此配置可获取 unionId
+        defaultBackUrl: '/account',
+    },
+};
