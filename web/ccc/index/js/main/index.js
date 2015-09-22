fa807b300c7bfe531b7523b4bcdaaba576012f42
jason <jason.xu@creditcloud.com>
2015-09-22 16:45:49 +0800

    fix

--- web/ccc/index/js/main/index.js
+++ web/ccc/index/js/main/index.js
@@ -203,7 +203,7 @@ request.get(encodeURI('/api/v2/cms/category/LINK/name/友情链接'))
     .then(function (res) {
         var count = new Ractive({
         el: '.firendLink',
-        template: '<span class="friend-left">友情链接</span><span class="friend-right">{{#each items}}<a href="http://{{url}}" target="_blank">{{{title}}}</a>{{/each}}</span>',
+        template: '<span class="friend-left" style="margin-right:16px;">友情链接</span><span class="friend-right">{{#each items}}<a href="http://{{url}}" target="_blank">{{{title}}}</a>{{/each}}</span>',
         data: {
             items: res.body
         }
