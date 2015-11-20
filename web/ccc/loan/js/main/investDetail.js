4df8cb68baf3cdc3f9d63b768178f8b538f6abd3
山东大学苏州研究院 <sdusz-02@shandongdaxuesuzhouyanjiuyuandeMac-mini-2.local>
2015-11-20 16:01:49 +0800

    bug

--- web/ccc/loan/js/main/investDetail.js
+++ web/ccc/loan/js/main/investDetail.js
@@ -54,9 +54,10 @@ function initailEasyPieChart() {
         var oldie = /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase());
         $(".easy-pie-chart").each(function () {
             var percentage = $(this).data("percent");
+            var status=$(this).data("status");
 			var percentageNum = CC.loan.rule.leftAmount;
             // 100%进度条颜色显示为背景色
-            var color = percentage === 100 ? "#f58220" : '#009ada';
+            var color = percentage != 100 && (status==='SETTLED'|| status==='CLEARED') ? "#f58220" : '#009ada';
             $(this).easyPieChart({
                 barColor: color,
                 trackColor: '#ddd',
