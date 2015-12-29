d331a6c8ae600b48e69d51abf1961207b86df1b3
zhouyongshan <yongshan.zhou@creditcloud.com>
2015-12-29 19:51:51 +0800

    限制产品显示最新一个

--- web/ccc/index/routers/page.js
+++ web/ccc/index/routers/page.js
@@ -102,9 +102,9 @@ function compare(propertyName){
 		var value1 = object1[propertyName];
 		var value2 = object2[propertyName];
 		if(value2 < value1){
-			return 1;
-		}else if(value2 > value1){
 			return -1;
+		}else if(value2 > value1){
+			return 1;
 		}else{
 			return 0;
 		}
