7902c2f78d084db889c699de4b946383c273664d
山东大学苏州研究院 <sdusz-02@shandongdaxuesuzhouyanjiuyuandeMac-mini-2.local>
2015-12-31 10:34:57 +0800

    用户须知

--- web/ccc/userKnow/routers/page.js
+++ web/ccc/userKnow/routers/page.js
@@ -3,14 +3,20 @@ module.exports = function (router) {
 
     router.get('/', function (req, res, next) /*data, locals, params, redirect)*/ {
         res.locals.title = '用户须知 | 新毅金融-基于位置的理财平台';
-        res.locals.latestOne = req.uest(encodeURI('/api/v2/cms/category/OTHER/name/用户须知'))
-        .end()
-        .get('body')
-        .then( function(data){
-           
-              _.assign(res.locals,{contents:data});
-        });
+        // res.locals.latestOne = req.uest(encodeURI('/api/v2/cms/category/OTHER/name/用户须知'))
+        // .end()
+        // .get('body')
+        // .then( function(data){
+        //
+        //       _.assign(res.locals,{contents:data});
+        // });
+        // req.data.articles('category/OTHER','name/用户须知').then(function(r){
+        //   // res.expose(r,'contents');
+        //   res.locals.contents=r;
+        // });
+        // res.expose(req.data.articles('category/OTHER','name/用户须知'),'contents');
+        res.locals.contents=req.data.articles('category/OTHER','name/用户须知');
 res.render()
     });
-  
+
 };
