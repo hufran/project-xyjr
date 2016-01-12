22457ea52b9e6572b6822da762cfb3c34b08fd8b
zhouyongshan <yongshan.zhou@creditcloud.com>
2016-01-12 20:18:17 +0800

    product link

--- web/src/ccc/invest/routers/page.js
+++ web/src/ccc/invest/routers/page.js
@@ -7,7 +7,7 @@ module.exports = function (router) {
         }
         res.expose(user, 'user');
         res.locals.title = '理财产品_718bank理财平台';
-        res.locals.keywords = '网络投资|P2P理财|个人理财|奇乐融投资理财|';
+        res.locals.keywords = '网络投资|P2P理财|个人理财|718bank投资理财|';
         res.locals.description =
             '718bank理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
 
@@ -19,6 +19,29 @@ module.exports = function (router) {
         res.locals.products.push(productKeyXSZX);
         res.locals.products.push(productKeyHDZX);
         res.locals.products.push(productKeyLCZQ);
+        res.expose('', 'product');
         res.render();
+    });
+router.get('/:product',async function (req, res) {
+        var user = res.locals.user;
+        if (user && user.idNumber) {
+            delete user.idNumber;
+        }
+        res.expose(user, 'user');
+        res.locals.title = '理财产品_718bank理财平台';
+        res.locals.keywords = '网络投资|P2P理财|个人理财|718bank投资理财|';
+        res.locals.description =
+            '718bank理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
+
+        var productKey = ['XSZX', 'HDZX', 'XNB'];
+        res.locals.products = [];
+        var productKeyXSZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).get('body');
+        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).get('body');
+        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).get('body');
+        res.locals.products.push(productKeyXSZX);
+        res.locals.products.push(productKeyHDZX);
+        res.locals.products.push(productKeyLCZQ);
+        res.expose(req.params.product, 'product')
+        res.render('index');
     });
 }
