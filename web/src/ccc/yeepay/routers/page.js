5d62fc9d3510ddfc52898db0870efcc6d2450e5b
zhouyongshan <yongshan.zhou@creditcloud.com>
2016-01-25 16:04:13 +0800

    fix

--- web/src/ccc/yeepay/routers/page.js
+++ web/src/ccc/yeepay/routers/page.js
@@ -88,7 +88,7 @@ module.exports = function (router) {
 	            req: req,
 	            body: req.body
 	        });
-            req.body.retUrl = req.headers.host + '/BankDepositReturn';
+            req.body.retUrl = req.headers.host + '/yeepay/BankDepositReturn';
 	        var data = qs.stringify(req.body);
 	        req.body = data.replace(/%5B\d+%5D/g, '');
 	        next();
