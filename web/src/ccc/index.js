faa01952191e69972c48de66a90993ab9fa5e9d6
node <node@licai.nnfe.local>
2016-03-04 07:17:26 -0800

    38 action

--- web/src/ccc/index.js
+++ web/src/ccc/index.js
@@ -173,6 +173,28 @@ _.each([
     });
 });
 
+_.each([
+    '/act'
+], function(url){
+    app.get(url, function (req, res, next) {
+        if (res.locals.user && res.locals.user.id) {
+            res.redirect('/act/success');
+        }
+        next();
+    });
+});
+
+_.each([
+    '/act/success'
+], function(url){
+    app.get(url, function (req, res, next) {
+        if (!(res.locals.user && res.locals.user.id)) {
+            res.redirect('/act');
+        }
+        next();
+    });
+});
+
 // mobile page (H5) redirection
 _.each([
     {path: '/'},
