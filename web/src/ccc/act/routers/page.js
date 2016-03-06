a3896adb84071f37de4d83e45e56eeff2f57d66f
node <node@licai.nnfe.local>
2016-03-06 07:48:27 -0800

    title

--- web/src/ccc/act/routers/page.js
+++ web/src/ccc/act/routers/page.js
@@ -4,6 +4,12 @@ var ccBody = require('cc-body');
 
 module.exports = function (router) {
     router.get('/', function (req, res, next) {
+		_.assign(res.locals, {
+            title : '3.8妇女节-718_金融理财平台',
+            keywords : '3.8妇女节-718_金融理财平台',
+            description : '3.8妇女节-718_金融理财平台'
+        });
+
         res.expose('/api/web/register/smsCaptcha', 'registerSmsCaptchaApi');
         res.expose('/api/web/register/voiceCaptcha', 'CC.registerVoiceCaptchaApi');
         res.expose('/api/web/register/submit', 'registerSubmit');
