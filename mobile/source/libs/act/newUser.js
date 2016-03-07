fc6d3c3a2b2f552932ea27827903c3217529f6bf
node <node@licai.nnfe.local>
2016-03-07 11:39:03 -0800

    38

--- mobile/source/libs/act/newUser.js
+++ mobile/source/libs/act/newUser.js
@@ -1,21 +1,24 @@
-jQuery(function(){
-	jQuery('.sider img').clone().appendTo('.sider');
-	jQuery('.sider img').eq(1).css('top','275');
-	
-	function gundong(){
-		var imgH1=parseInt(jQuery('.sider img').eq(0).css('top'));
-		var imgH2=parseInt(jQuery('.sider img').eq(1).css('top'));
-		if (imgH1>-275) {
-			jQuery('.sider img').animate({top: '-=55'},500);
-		}else if(imgH1==-275){
-				jQuery('.sider img').eq(0).remove();
-				jQuery('.sider img').clone().appendTo('.sider');
-				jQuery('.sider img').eq(1).css('top','275');
-			
-		}
-	}
-		var t1 = window.setInterval(gundong,1000); 
 
-	 
-	
+jQuery(function(){
+		tclone();
+		var t1 = window.setInterval(gundong,1000); 
 })
+function gundong(){
+	var slideHeight = jQuery('.sider').height();
+	var maxH = slideHeight*4;
+
+	var imgH1=parseInt(jQuery('.sider img').eq(0).css('top'));
+	var imgH2=parseInt(jQuery('.sider img').eq(1).css('top'));
+	if (imgH1>-maxH) {
+		jQuery('.sider img').animate({top: '-='+slideHeight},500);
+	}else if(imgH1<=-maxH){
+			tclone();
+			jQuery('.sider img').eq(0).remove();
+	}
+}
+function tclone(){
+	jQuery('<img src="assets/image/act/sider.png">').appendTo('.sider');
+
+	jQuery('.sider img').eq(1).css('top','0px');
+	
+}
