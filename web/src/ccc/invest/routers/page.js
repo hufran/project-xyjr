dad91a31b4715371f4a8aefffec375fe05a53e6d
renhg <renhongguang@nnfe.com>
2016-03-14 15:59:23 -0800

    718bank

--- web/src/ccc/invest/routers/page.js
+++ web/src/ccc/invest/routers/page.js
@@ -11,14 +11,15 @@ module.exports = function (router) {
         res.locals.description =
             '718金融理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
 
-        var productKey = ['XNB', 'FB', 'XJB'];
+        //var productKey = ['XNB', 'FB', 'XJB'];
+		var productKey = ['XNB'];
         res.locals.products = [];
         var productKeyXSZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
-        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
-        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        //var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
+        //var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
         res.locals.products.push(productKeyXSZX);
-        res.locals.products.push(productKeyHDZX);
-        res.locals.products.push(productKeyLCZQ);
+       // res.locals.products.push(productKeyHDZX);
+       // res.locals.products.push(productKeyLCZQ);
         res.expose('', 'product');
         res.render();
         return false;
@@ -29,19 +30,20 @@ router.get('/:product', async function (req, res) {
             delete user.idNumber;
         }
         res.expose(user, 'user');
-        res.locals.title = '理财产品_718金融·理财平台';
-        res.locals.keywords = '网络投资|P2P理财|个人理财|718金融·投资理财|';
+        res.locals.title = '理财产品_718bank理财平台';
+        res.locals.keywords = '网络投资|P2P理财|个人理财|718bank投资理财|';
         res.locals.description =
-            '718金融·理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
+            '718bank理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
 
-        var productKey = ['XNB', 'FB', 'XJB'];
+        //var productKey = ['XNB', 'FB', 'XJB'];
+		var productKey = ['XNB'];
         res.locals.products = [];
         var productKeyXSZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
-        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
-        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
+        //var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
+        //var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
         res.locals.products.push(productKeyXSZX);
-        res.locals.products.push(productKeyHDZX);
-        res.locals.products.push(productKeyLCZQ);
+        //res.locals.products.push(productKeyHDZX);
+        //res.locals.products.push(productKeyLCZQ);
         res.expose(req.params.product, 'product')
         res.render('index');
         return false;
