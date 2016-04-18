2fa148ccc44da599368f40ded5d7ac5b7cb097b6
zzd <zzd@nnfe.com>
2016-04-18 17:52:54 +0800

    红包

--- web/src/ccc/newAccount/js/main/coupon.js
+++ web/src/ccc/newAccount/js/main/coupon.js
@@ -17,13 +17,6 @@ var getCurrentType = function() {
 $('ul.ttabs li a').on('click', function() {
 	var type = $(this).parent().data('type');
 	init(type);
-
-	//getcouponId(); sss
-	jQuery('#zhuangtai-'+type).val('');
-	if (type='REBATE') {
-		jQuery('#huoqu-'+type).val('');
-	}
-	
 });
 
 Date.prototype.Format = function (fmt) { //author: meizz
@@ -46,15 +39,14 @@ function init (type) {
 	console.log(type);
 	if (type) {
 		var couponRactive = new Ractive ({
-			el: '.bodypanel-' + type,
+			el: '.panel-' + type,
 			template: couponTpl,
 			size: pagesize,
             perpage:self.size,
 			page: page,
 			totalPage: totalPage,
-			//totalMoney:
-
-            api:'/api/v2/rebateCounpon/listUserCouponPlacementByCond/'+CC.user.userId,
+			//api: '/api/v2/coupon/MYSELF/coupons',
+			api:'/api/v2/coupon/'+CC.user.userId+'/coupons/byStatus',
 			data: {
 				loading: true,
 				list: [],
@@ -63,13 +55,11 @@ function init (type) {
 			bindTime:0,
 			status: {
 	            'INITIATED': '未使用',
-	            'ACHIEVE_UP': '已领完',
 	            'PLACED': '未使用',
-	            'USED': '已使用',
+				'USED': '审核中',
 	            'CANCELLED': '已作废',
 	            'EXPIRED': '已过期',
-	            'REDEEMED': '已使用',
-	            'USING' :'使用中',
+				'REDEEMED': '已使用'
 	        },
 	        type: {
 	            'CASH': '现金券',
@@ -80,81 +70,31 @@ function init (type) {
 
 			onrender: function() {
 				var self = this;
-				var isClick=false;
-				
-				if (isClick==false) {
 						self.getCouponData(function (o){
 						self.set('total',o.totalSize);
-						self.actualAmount(o.results);
 	                    var parseResult = self.parseData(o.results);
-	                    console.log('parseResult-----',parseResult); 
+//					self.setData(self.parseData(o.results));
+	                    console.log('parseResult-----',parseResult);
 						self.setData(parseResult);
-						jQuery('.xiangxi').removeClass('dtr').addClass('dn');
-						jQuery('td[colspan="7"] tbody').html('');
-						getcouponId();
 					});
-				}
-				
-				//点击查询红包按钮start
-				var getcouponIdClick=true;
-				jQuery('.chaxunBtn-'+type).click(function(){
-					isClick=true;					
-					self.getCouponCond(function(z){
-	                    var chaxun = self.parseData(z.results);
-	                    self.set('total',z.totalSize);
-						self.setData(chaxun);
-						 if(getcouponIdClick){
-							getcouponId();
-							getcouponIdClick=false;
-						 }
-					})
-					//event.stopPropagation();
-				})
-				//点击查询红包按钮 end
-				
 				if (self.bindTime == 0) {
 					self.initClick();
 					self.bindTime ++;
 				}
-
-			},
-			//根据状态查询红包
-			getCouponCond:function (callback){
-					var self = this;
-					var couponPackageId='';
-					var statusCha=jQuery('#zhuangtai-'+type).find("option:selected").val();
-					if (type=='REBATE') {
-						couponPackageId=jQuery('#huoqu-REBATE').find("option:selected").val();
-						$.post("/api/v2/rebateCounpon/getUserCouponPlacementsByCond/"+CC.user.userId,{
-							type:type,
-							page:page-1,
-							couponPackageId:couponPackageId,
-							status:statusCha
-						},function(z){
-						    callback(z);
-						});
-					}else{
-						$.post("/api/v2/rebateCounpon/getUserCouponPlacementsByCond/"+CC.user.userId,{
-							type:type,
-							page:page-1,
-							status:statusCha
-						},function(z){
-						    callback(z);
-						});
-					}
-					
-					
 			},
 			getCouponData: function(callback) {
 				var self = this;
-				$.post(self.api,{
-					type:type,
-					page: (self.page - 1) ,
+
+				$.get(self.api,{
+					status: type,
+					pageNo: self.page,
 					pageSize: self.size
 				},function (o){
-					if (o.results.length>0) {
-						self.pageOneData = o.results;
-						callback(o);
+
+					console.log(o.data);
+					if (o.success) {
+						self.pageOneData = o.data.results;
+						callback(o.data);
 					}
 				});
 			},
@@ -164,31 +104,19 @@ function init (type) {
 				self.set('list', o);
 				self.renderPager();
 			},
-			actualAmount:function(o){
-				jQuery('#actualAmount').html(o[0].actualAmount);
-				jQuery('#priv').html(o[0].priv);
-			},
 			parseData: function(o) {
 	            for (var i = 0; i < o.length; i++) {
-	            	if(o[i].couponPackage!=null){
 	            		o[i].displayName = o[i].couponPackage.displayName;
 	            		o[i].parValue = o[i].couponPackage.parValue;
 	                	o[i].type = o[i].couponPackage.type;
 	                	o[i].typeKey = o[i].couponPackage.displayName;
-	                	o[i].minimumInvest = o[i].couponPackage.minimumInvest;
-	                	o[i].minimumDuration = o[i].couponPackage.minimumDuration;
-	                	o[i].parValue=parseFloat(o[i].couponPackage.parValue).toFixed(2);
-	            	}
-	                
-                    o[i].actualAmount=parseFloat(o[i].actualAmount).toFixed(2);
-                    o[i].ableAmount=parseFloat(o[i].parValue-o[i].actualAmount).toFixed(2);
 	                o[i].canuse = false;
 	                if (o[i].type === 'CASH') {
 	                    if (o[i].status === 'INITIATED' || o[i].status === 'PLACED') {
 	                        o[i].canuse = true;
 	                    }
 	                }
-	                if (o[i].type === 'INTEREST') {//加息
+					if (o[i].type === 'INTEREST') {
 	                	o[i].interest  = true;
 	                    o[i].displayValue = (parseFloat(o[i].parValue)/100).toFixed(2) + '%';
 	                } else if (o[i].type === 'CASH') {
@@ -203,13 +131,7 @@ function init (type) {
 	                    o[i].displayStatus = '未使用';
 	                } else if (o[i].status === 'USED') {
 	                	o[i].USED = true;
-	                    o[i].displayStatus = '已使用';
-	                } else if (o[i].status === 'ACHIEVE_UP') {
-	                	o[i].USED = true;
-	                    o[i].displayStatus = '已领完';
-	                } else if (o[i].status === 'USING') {
-	                	o[i].USED = true;
-	                    o[i].displayStatus = '使用中';
+						o[i].displayStatus = '审核中';
 	                } else if (o[i].status === 'REDEEMED') {
 	                	o[i].REDEEMED = true;
 	                    o[i].displayStatus = '已兑换';
@@ -218,16 +140,15 @@ function init (type) {
 	                    o[i].displayStatus = '已过期';
 	                } else if (o[i].status === 'CANCELLED') {
 	                	o[i].CANCELLED = true;
-	                    o[i].displayStatus = '已作废';
+						o[i].displayStatus = '已失效';
 	                }
 	                o[i].used = false;
 	                if (o[i].status === 'USED' || o[i].status === 'REDEEMED') {
 	                    o[i].used = true;
 	                }
-	                o[i].status = this.status[o[i].status];
+//	                o[i].status = this.status[o[i].status];
 	                o[i].timePlaced = (new Date(o[i].timePlaced)).Format("yyyy-MM-dd");//分发时间
 	                o[i].timeRedeemed = o[i].timeRedeemed;//兑换时间
-	                if(o[i].couponPackage!=null){
 	                	o[i].description = o[i].couponPackage.description;
 		                o[i].totalAmount = o[i].couponPackage.totalAmount;
 		                o[i].timeIssued = o[i].couponPackage.timeIssued;
@@ -237,8 +158,6 @@ function init (type) {
 		                } else {
 		                	o[i].timeExpire = (new Date(o[i].couponPackage.timeExpire)).Format("yyyy-MM-dd");
 		                }
-		            }
-	                
                     if(o[i].timeExpire != "永不过期"){
 						if(o[i].displayStatus === '未使用'){
 							if(o[i].couponPackage.timeExpire<new Date()){
@@ -262,7 +181,7 @@ function init (type) {
 				var self = this;
 				var currentPage = $(".currentPage").text();
 				$(".prev").click(function(){
-					if ( self.page == 0 ) {
+					if ( self.page == 1 ) {
 						return false
 					} else {
 						self.page = self.page - 1;
@@ -282,11 +201,13 @@ function init (type) {
 			renderPager: function () {
                 var self = this;
                 var totalSize = self.get('total');
+
                 if (totalSize != 0) {
                     self.totalPage = Math.ceil(totalSize / self.size);
                 }
 
                 var totalPage = [];
+				console.log("===>> totalPage = " + self.totalPage);
                 for (var i = 0; i < self.totalPage; i++) {
                     totalPage.push(i+1);
                 }
@@ -312,11 +233,12 @@ function init (type) {
             pagerRactive.on('previous', function (e) {
                 e.original.preventDefault();
                 var current = this.get('current');
-                if (current > 0) {
+				if (current > 1) {
                     current -= 1;
                     this.set('current', current);
                     couponRactive.page = current;
                     couponRactive.onrender();
+
                 }
             });
 
@@ -341,6 +263,7 @@ function init (type) {
                     this.set('current', current);
                     couponRactive.page = current;
                     couponRactive.onrender();
+
                 }
             });
         }
@@ -350,69 +273,6 @@ function init (type) {
 
 //var ctype=['CASH','INTEREST','PRINCIPAL','REBATE'];
 init(getCurrentType());
-
-//点击查看红包详情
-window.onload=function(){
-	CouponPackage();
-}
-function couponInfData(Id,callback){
-	$.get("/api/v2/rebateCounpon/getRebateCouponRecordsByCouponId/"+Id, {couponId:Id },
-  function(o){
-    callback(o);
-  });
-}
-function getcouponId(event){
-	$('.checkInforBtn').click(function(){
-		var couponId='';
-		couponId=jQuery(this).siblings('.couponId').text();
-		if (jQuery('#'+couponId).parents('.xiangxi').hasClass('dn')) {
-			couponInfData(couponId,function(o){
-				var trHtml="";
-				for (var i = 0; i < o.data.length; i++) {
-					o.data[i].useTimeDate=(new Date(o.data[i].useTime)).Format("yyyy-MM-dd");
-					o.data[i].amount=o.data[i].amount;
-					trHtml=trHtml+'<tr><td style="text-indent:20px;">'+o.data[i].useTimeDate+'</td><td>'+o.data[i].amount+'</td><td>'+'投资'+'</td></tr>';
-				}
-				jQuery('#'+couponId).html(trHtml);
-				jQuery('#'+couponId).parents('.xiangxi').removeClass('dn').addClass('dtr');
-			});
-		}else{
-			jQuery('#'+couponId).parents('.xiangxi').removeClass('dtr').addClass('dn');
-		}
-		event.stopPropagation();
-	})
-
-}
-
-// function stopPropagation(evt) {
-//     if (stopPropagation!= undefined) {
-//         evt.stopPropagation();
-//     } else {
-//         evt.cancelBubble = true;
-//     }
-// }
-
-//奖券获取原因下来列表
-function getUsingCouponPackage(callback){
-	$.get("/api/v2/rebateCounpon/getUsingCouponPackage",
-  function(o){
-    callback(o);
-  });
-}
-function CouponPackage(){
-	getUsingCouponPackage(function(o){
-		var optionHtml='<option value="">全部</option>';
-		for (var i = 0; i < o.data.length; i++) {
-			if(o.data[i].type='REBATE'){
-				var optionHtml=optionHtml+'<option value='+o.data[i].id+'>'+o.data[i].displayName+'</option>';
-			}
-		}
-		// if (jQuery('#huoqu-REBATE').html()=false) {
-		 	jQuery('#huoqu-REBATE').html(optionHtml);
-		// }
-		
-	})
-}
 
 window.redeemCoupon = function(btn) {
     var id = $(btn).data("id");
@@ -427,4 +287,3 @@ window.redeemCoupon = function(btn) {
         }
     });
 }
-
