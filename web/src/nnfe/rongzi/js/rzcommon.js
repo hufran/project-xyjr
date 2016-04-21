418408c81fcecb12de6114c6f304355db9923c46
zzd <zzd@nnfe.com>
2016-04-21 09:55:11 +0800

    企业融资

---
+++ web/src/nnfe/rongzi/js/rzcommon.js
@@ -0,0 +1,6 @@
+$.get("/project-xyjr/web/src/nnfe/global/header.html",function(data){
+		$(".header").html(data);
+	});
+$.get("/project-xyjr/web/src/nnfe/global/footer.html",function(data){
+	$(".footer").html(data);
+});
