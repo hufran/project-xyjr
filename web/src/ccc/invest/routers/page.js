6825f37f0f5bb5291cf037e4ce19a57fc8e19de4
Jude <zhuzhidan123@gmail.com>
2016-05-25 19:11:11 +0800

    金裘保提测落下一个文件

--- web/src/ccc/invest/routers/page.js
+++ web/src/ccc/invest/routers/page.js
@@ -10,20 +10,21 @@ module.exports = function (router) {
         res.locals.keywords = '理财产品、投资、理财投资、个人理财、理财新品、新能宝、活动专享、新手专享';
         res.locals.description =
             '718金融理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
+        req.uest('/api/v2/navigation/listDisplayProductForPc/pc')
+            .then(function (resData){
+                
+                res.locals.products =[];
+                var products0 ={"data":resData.body[0]};
+                var products1 ={"data":resData.body[1]};
+                var products2 ={"data":resData.body[2]};
 
-        //var productKey = ['XNB', 'FB', 'XJB'];
-		//var productKey = ['XNB', 'HDZX','XSZX'];
-        var productKey = ['XNB', 'HDZX','XJB'];
-        res.locals.products = [];
-        var productKeyXJB=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
-        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
-        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
-        res.locals.products.push(productKeyXJB);
-		res.locals.products.push(productKeyHDZX);
-		res.locals.products.push(productKeyLCZQ);
-        res.expose('', 'product');
-        res.render();
-        return false;
+                res.locals.products.push(products0);
+                res.locals.products.push(products1);
+                res.locals.products.push(products2);
+                res.expose('', 'product');
+                res.render();
+                return false;
+            })
     });
 router.get('/:product', async function (req, res) {
         var user = res.locals.user;
@@ -35,18 +36,20 @@ router.get('/:product', async function (req, res) {
         res.locals.keywords = '网络投资|P2P理财|个人理财|718bank投资理财|';
         res.locals.description =
             '718bank理财平台为您提供了多种理财产品，每种理财产品都有不同的特点，满足您的投资需求。理财产品有：新手专享、活动专享、新能宝等。';
+        res.expose(req.params.product, 'product');
+        req.uest('/api/v2/navigation/listDisplayProductForPc/pc')
+            .then(function (resData){
+                res.locals.products=[];
+                var products0 ={"data":resData.body[0]};
+                var products1 ={"data":resData.body[1]};
+                var products2 ={"data":resData.body[2]};
 
-        //var productKey = ['XNB', 'FB', 'XJB'];
-		//var productKey = ['XNB', 'HDZX','XSZX'];
-        var productKey = ['XNB', 'HDZX','XJB'];
-        res.locals.products = [];
-        var productKeyXJB=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[0]).end().get('body');
-        var productKeyHDZX=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[1]).end().get('body');
-        var productKeyLCZQ=await req.uest('/api/v2/loan/getLoanProduct/productKey/'+productKey[2]).end().get('body');
-        res.locals.products.push(productKeyXJB);
-        res.locals.products.push(productKeyHDZX);
-        res.locals.products.push(productKeyLCZQ);
-        res.render('index');
-        return false;
+                res.locals.products.push(products0);
+                res.locals.products.push(products1);
+                res.locals.products.push(products2);
+            })
+            res.render('index');
+            return false;
     });
+
 }
