2f372329cddcf06810abc00cffbd5322b57c4968
dwy <272632013@qq.com>
2017-02-16 16:46:42 +0800

    app端调用h5的静态页面新增

--- mobile/source/libs/act/couponList.js
+++ mobile/source/libs/act/couponList.js
@@ -223,3 +223,85 @@ function isUsefulToken(userId,token){
 	    	};
 	});
 }
+
+//新增app端页面js
+//pulicnumber页面调试
+function publicTest(){
+	//var swiper = new Swiper('.swiper-container', {
+	//	pagination: '.swiper-pagination',
+	//	paginationClickable: true,
+	//	slideToClickedSlide:true,
+	//	slidesPerView: 5,
+	//	spaceBetween: 1,
+	//	scrollbar: '.swiper-scrollbar',
+	//	breakpoints: {
+	//		1024: {
+	//			slidesPerView: 3.5,
+	//			spaceBetween: 0
+	//		},
+	//		768: {
+	//			slidesPerView: 3.5,
+	//			spaceBetween: 0
+	//		},
+	//		640: {
+	//			slidesPerView: 3.5,
+	//			spaceBetween: 0
+	//		},
+	//		320: {
+	//			slidesPerView: 3.5,
+	//			spaceBetween: 0
+	//		}
+	//	}
+	//});
+	var mySwiper2 = new Swiper('#swiper-container2',{
+		watchSlidesProgress : true,
+		watchSlidesVisibility : true,
+		slidesPerView : 3,
+		slideToClickedSlide:true,
+		onTap: function(){
+			mySwiper3.slideTo( mySwiper2.clickedIndex)
+		},
+		breakpoints: {
+			1024: {
+				slidesPerView: 4.5,
+				spaceBetween: 0
+			},
+			768: {
+				slidesPerView: 4.5,
+				spaceBetween: 0
+			},
+			640: {
+				slidesPerView: 4.5,
+				spaceBetween: 0
+			},
+			320: {
+				slidesPerView: 4.5,
+				spaceBetween: 0
+			}
+		}
+	})
+	var mySwiper3 = new Swiper('#swiper-container3',{
+
+		onSlideChangeStart: function(){
+			updateNavPosition()
+		}
+
+	})
+
+	function updateNavPosition(){
+		$('#swiper-container2 .active-nav').removeClass('active-nav')
+		var activeNav = $('#swiper-container2 .swiper-slide').eq(mySwiper3.activeIndex).addClass('active-nav');
+console.log("updateNavP")
+
+		if (!activeNav.hasClass('swiper-slide-visible')) {
+			if (activeNav.index()>mySwiper2.activeIndex) {
+				console.log(1);
+				var thumbsPerNav = Math.floor(mySwiper2.width/activeNav.width())-1
+				mySwiper2.slideTo(activeNav.index()-thumbsPerNav)
+			}else {
+				console.log(2);
+				mySwiper2.slideTo(activeNav.index())
+			}
+		}
+	}
+}
