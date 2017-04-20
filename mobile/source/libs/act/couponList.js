79f63b190e0aec1642a48b5161a5d17883844eb2
dwy <272632013@qq.com>
2017-04-20 10:54:14 +0800

    pc端不能添加充值10.02这样的数据修改

--- mobile/source/libs/act/couponList.js
+++ mobile/source/libs/act/couponList.js
@@ -210,17 +210,48 @@ function isWeiXin(){
 	}
 }
 
+var cookieFn={
+	setCookie:function (objName, objValue, objHours){//添加cookie
+		var str = objName + "=" + escape(objValue);
+		if (objHours > 0) {//为0时不设定过期时间，浏览器关闭时cookie自动消失
+			var date = new Date();
+			var ms = objHours * 3600 * 1000;
+			date.setTime(date.getTime() + ms);
+			str += "; expires=" + date.toGMTString();
+		}
+		console.log(str+"; path=/");
+		document.cookie = str+"; path=/";
+	},
+
+	getCookie:function (objName){//获取指定名称的cookie的值
+		var arrStr = document.cookie.split("; ");
+		for (var i = 0; i < arrStr.length; i++) {
+			var temp = arrStr[i].split("=");
+			if (temp[0] == objName)
+				return unescape(temp[1]);
+		}
+	},
+	cleanCookie:function (name){//为了删除指定名称的cookie，可以将其过期时间设定为一个过去的时间
+		var date = new Date();
+		date.setTime(date.getTime() - 10000);
+		document.cookie=name+"=; expire="+date.toGMTString()+"; path=/";
+	}
+}
 
 //判断token是否过期
 function isUsefulToken(userId,token){
 	console.log('userId='+userId+'&token='+token);
 	$.get("/api/mermaid/users/checkToken?userId="+userId+'&token='+token, function(res){
+		console.log(1);
 	    console.log(res);
 	    	if (res.status!=0) {
 	    		console.log($('#jumpToJd').html());
 	    		$('#jumpToJd').html(res.msg);
 	    		console.log(11111+$('#jumpToJd').html());
-	    	};
+	    	}else{
+				//检测是否存在cookie
+				cookieFn.setCookie('ccat',token);
+			}
 	});
 }
 
@@ -300,4 +331,5 @@ function c1(){
 	$("#swiper-container3,#swiper-container3 .swiper-wrapper").css("height",slider[0]+"px");
 	$("#swiper-container3.swiper-container").css("overflow","hidden");
 }
+
 
