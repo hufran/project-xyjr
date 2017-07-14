677c20d81e7ddef225b11d9e0ee7be11b384eabc
feiqp <feiqingping@nnfe.com>
2017-07-14 15:51:18 +0800

    投资

--- web/src/ccc/loan/js/main/investDetail.js
+++ web/src/ccc/loan/js/main/investDetail.js
@@ -192,10 +192,11 @@ setTimeout((function () {
     }
 
 
-
+    var mark;
     if (CC.user) {
         accountService.getUserInfo(function (res) {
             investRactive.set('name', res.user.name);
+            mark = res.user.priv;
         });
     }
 
@@ -308,7 +309,8 @@ setTimeout((function () {
 					if (document.getElementById('agree').checked == true){
                          // 问卷start
                          // if (CC.user.priv==null&&getCookie('question')==null)
-                        if (CC.user.priv==null) {
+                         console.log("mark:" + mark);
+                        if (!mark) {
                             jQuery('.wenjuan').removeClass('dn').addClass('db');
                             jQuery('.radioW').click(function(){
                                 var radioName=jQuery(this).siblings('input[type="radio"]').prop('name');
