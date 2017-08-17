42316b0a78142d544b9eedd4271cea1e14a07b4e
hufuchen <hufuchen@nnfe.com>
2017-08-17 17:52:58 +0800

    新需求变更

--- web/src/ccc/index.js
+++ web/src/ccc/index.js
@@ -30,7 +30,6 @@ ds.request(app, config.urlBackend);
 var Data = require('@ds/data');
 app.use(function(req,res,next){
     req.data = new Data(req);
-    res.setHeader("X-Frame-Options", "SAMEORIGIN");
     next();
 });
 app.use('/api/web', ds.loader('api'));
